530dc59044b983f9ae898c46c217341fe6f8ce80
chris <chris.bhall@outlook.com>
2022-03-20 08:57:13 +0000

    complete user validations before registration

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    # validation tests/examples here
+    it 'validates password' do
+      user = User.new(password:"bill").valid?
+    end
+    it 'validates password confirmation' do
+      user = User.new(password_confirmation:"bill").valid?
+  end
+    it 'validates password & password confirmation are the same' do
+      user = User.new(password: "bob", password_confirmation:"bob")
+      expect(user.password).to eq(user.password_confirmation)
+    end
+end
 end
